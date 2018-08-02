69651e053a830ad9a6067ba88ab8313157bf150c
CharlesKocher <charles_kocher@brown.edu>
2018-08-02 10:29:01 -0400

    Update ckalgomethods.py
    Removed inaccurate licensing, added some description.

--- qiskit-env/ckalgomethods.py
+++ qiskit-env/ckalgomethods.py
@@ -1,21 +1,5 @@
-# -*- coding: utf-8 -*-
-
-# Copyright 2018 IBM.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =============================================================================
-
-"""Algorithm functions for running etc. Takes code from algomethods.py in ACQUA"""
+# Modified from QISKit ACQUA
+"""Algorithm function for retrieving an instance of the algorithm etc. Takes code from algomethods.py in ACQUA"""
 
 from qiskit_acqua.algorithmerror import AlgorithmError
 from qiskit_acqua._discover import (_discover_on_demand,
@@ -32,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 def get_algorithm_ck(params, algo_input=None, json_output=False):
     """
-    Run algorithm as named in params, using params and algo_input as input data
+    Retrieve algorithm as named in params, using params and algo_input as input data
     and returning a result dictionary
     Args:
         params (dict): Dictionary of params for algo and dependent objects
